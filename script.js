7d7fb5f0d54e99223466ec0ebfbd99b324b63883
Victorvalour <ewehvictor7@gmail.com>
2023-08-15 03:31:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const orderButton = document.querySelector(".general-btn");
+orderButton.onclick = () => alert("Your order has been placed.")
+
+const orderButton2 = document.querySelector(".order-btn");
+orderButton2.onclick = () => alert("Your order has been placed.")
+
+onscroll
