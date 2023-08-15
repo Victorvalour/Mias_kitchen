0d2b159d26c0e1f72fff01c6826f07c22a7a2e52
Victorvalour <ewehvictor7@gmail.com>
2023-08-16 00:28:12 +0100

    new commit

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ orderButton.onclick = () => alert("Your order has been placed.")
 const orderButton2 = document.querySelector(".order-btn");
 orderButton2.onclick = () => alert("Your order has been placed.")
 
-onscroll
+const menuButton = document.querySelector('.hamburger-icon-orange');
+const menuPanel = document.querySelector('.menu-panel');
+const body = document.querySelector('body')
+if (menuPanel.style.display ="none") {
+menuButton.onclick = () => menuPanel.style.display ="block"}
+ 
+ onscroll = () => menuPanel.style.display ="none";
