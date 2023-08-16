7346d89dca8d880e056c55e09a84c2dc5779815f
Victorvalour <ewehvictor7@gmail.com>
2023-08-16 05:05:09 +0100

    new commit

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const orderButton = document.querySelector(".general-btn");
-orderButton.onclick = () => alert("Your order has been placed.")
 
 const orderButton2 = document.querySelector(".order-btn");
 orderButton2.onclick = () => alert("Your order has been placed.")
 
-const menuButton = document.querySelector('.hamburger-icon-orange');
-const menuPanel = document.querySelector('.menu-panel');
+const menuButton = document.querySelector('.sidebar-button');
+const menuPanel = document.querySelector('#sidebar-panel');
 const body = document.querySelector('body')
 if (menuPanel.style.display ="none") {
 menuButton.onclick = () => menuPanel.style.display ="block"}
